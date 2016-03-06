1af10b0d7bf421dd09a8c5b19b1face343b24f40
joseph muli <jayjaymuli00@gmail.com>
2016-03-06 22:42:40 +0300

    added activities

---
+++ app/src/main/java/com/espot/ent/entertainmentspot/Animations.java
@@ -0,0 +1,13 @@
+package com.espot.ent.entertainmentspot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Animations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.animations);
+    }
+}
